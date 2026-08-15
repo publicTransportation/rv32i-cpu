3ae78bca9b0e655468706b388cea8b7a9a816605
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-08-14 21:01:11 -0400

    feat(imm_gen) add basic imm gen module supporting I, S, B type instructions

--- rtl/common/imm_gen.sv
+++ rtl/common/imm_gen.sv
@@ -1,17 +1,26 @@
-module imm_gen (
+// Supports I, S, B type instructions
+// Missing: U, J type
+module imm_gen 
+    import rv32i_pkg::*;
+(
     input logic [31:0] instr,
     output logic [31:0] signext_imm
 );
 // Decode instr[6:0] locally to avoid control unit path dependency
-case (instr[6:2])
-    case1 :
-    default:
-
-endcase
-
 // Extract 12 imm bits
-
 // Sign extend to 32 bits (rv32i)
+always_comb begin // Models combinational logic
+    case (instr[6:0])
+        OPCODE_OP_IMM, 
+        OPCODE_JALR,
+        OPCODE_LOAD: signext_imm = {{20{instr[31]}}, instr[31:20]}; // I-type
 
+        OPCODE_STORE: signext_imm = {{20{instr[31]}}, instr[31:25], instr[11:7]}; // S-type
+
+        OPCODE_BRANCH: signext_imm = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0}; // B-type, grounded LSB 
+
+        default: signext_imm = 32'b0;
+    endcase
+end
 
 endmodule
