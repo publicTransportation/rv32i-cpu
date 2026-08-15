7a47af268323f2cf7cace92a4ed76a6612d67826
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-08-15 07:16:35 -0400

    wip(core_single): start barebones top level core wrapper

---
+++ rtl/common/core_single.sv
@@ -0,0 +1,16 @@
+module core_single // Top level wrapper
+(
+    input logic clk,
+    input logic rst_n,
+
+    // Instruction memory interface
+
+
+
+    // Data memory interface
+
+
+
+);
+
+endmodule
