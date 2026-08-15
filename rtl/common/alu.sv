b827fc1278d0a2d0457232b9f1a0e5f8c99d08b3
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-08-15 07:26:13 -0400

    wip(alu): start barebones alu module

---
+++ rtl/common/alu.sv
@@ -0,0 +1,11 @@
+module alu
+(
+    input logic A,
+    input logic B, 
+    output logic zero,
+    output logic 
+);
+
+//case statement, possibly always @() combinational logic could need trigger for ALU unit
+
+endmodule
