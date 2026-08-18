61742e37bb84227574dc86273e808bd4da57da9b
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-08-18 12:22:47 -0400

    wip(reg_file) rough working on rf data structure as unpacked array

--- rtl/common/reg_file.sv
+++ rtl/common/reg_file.sv
@@ -1,18 +1,29 @@
 module reg_file 
-
+    import rv32i_pkg::*; // needed?
 (
+    input logic clk,
+    input logic rst_n,
     input logic reg_write,
 
-    input logic rs1,
-    input logic rs2,
-    input logic rd,
+    input logic [4:0] rs1,
+    input logic [4:0] rs2,
+    input logic [4:0] rd,
     
-    input logic write_data,
+    input logic [31:0] write_data,
     
-    output logic read_data1,
-    output logic read_data2
+    output logic [31:0] read_data1,
+    output logic [31:0] read_data2
 );
 
+logic [31:0] rf [0:31];
 
+always_comb begin
+    read_data1 = rf[rs1];
+    read_data2 = rf[rs2];
+end
+
+always @() begin
+    
+end
 
 endmodule
