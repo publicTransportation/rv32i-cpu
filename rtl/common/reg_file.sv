b9bbeaeb3c869a2a594a6dafc7b9e7d8783598c5
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-08-18 12:14:31 -0400

    wip(reg_file) created reg_file, sketching data structure to hold registers

---
+++ rtl/common/reg_file.sv
@@ -0,0 +1,18 @@
+module reg_file 
+
+(
+    input logic reg_write,
+
+    input logic rs1,
+    input logic rs2,
+    input logic rd,
+    
+    input logic write_data,
+    
+    output logic read_data1,
+    output logic read_data2
+);
+
+
+
+endmodule
