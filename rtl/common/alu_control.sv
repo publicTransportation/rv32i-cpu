2371e9f8a3b7e5ab17ef02ed6be116cf9cf32d2d
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-08-19 17:04:48 -0400

    feat(alu_control): add decoding support for basic instructions ADD, SUB, AND, OR, SLT

--- rtl/common/alu_control.sv
+++ rtl/common/alu_control.sv
@@ -1,7 +1,7 @@
 module alu_control 
     import rv32i_pkg::*;
 (
-    input rv32i_pkg::opcode_e opcode,
+    //input rv32i_pkg::opcode_e opcode,
     input logic [1:0] alu_op,
 
     input  logic [6:0] funct7, // Instruction indices [31:25]
@@ -10,18 +10,20 @@ module alu_control
     output rv32i_pkg::alu_ctrl_e alu_ctrl
 );
 
-always_comb begin // Using P&H p.502 Figure 4.13
+always_comb begin 
     if (alu_op[1]) begin
         case ({funct7, funct3})
-            10'b0000000_000: alu_ctrl = ALU_SLL;
-            10'b0100000_000: alu_ctrl = ALU_SRL;
-            10'b0000000_111: alu_ctrl = ALU_ADD;
-            10'b0000000_110: alu_ctrl = ALU_SUB;
+            10'b0000000_000: alu_ctrl = ALU_ADD;
+            10'b0100000_000: alu_ctrl = ALU_SUB;
+            10'b0000000_111: alu_ctrl = ALU_AND;
+            10'b0000000_110: alu_ctrl = ALU_OR; // First 4 come from P&H p.502 Figure 4.13
+            10'b0000000_010: alu_ctrl = ALU_SLT;
+            default: alu_ctrl = ALU_ADD;
         endcase
     end else if (alu_op[0]) begin
-        alu_ctrl = ALU_SRL;
+        alu_ctrl = ALU_SUB; // Branch
     end else begin
-        alu_ctrl = ALU_SLL;
+        alu_ctrl = ALU_ADD; // Load / Store
     end
 end
 
