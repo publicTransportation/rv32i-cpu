4ddd14ce68c675272f887905cd4fb954dd890a6a
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-08-19 18:16:20 -0400

    feat(alu) implement alu operations mapped to the 4 digit alu ctrl code

--- rtl/common/alu.sv
+++ rtl/common/alu.sv
@@ -3,7 +3,7 @@ module alu
 (
     input logic [XLEN-1:0] A,
     input logic [XLEN-1:0] B,
-    input alu_ctrl_e ctrl,
+    input rv32i_pkg::alu_ctrl_e ctrl,
 
     output logic zero,
     output logic [XLEN-1:0] result 
@@ -11,19 +11,18 @@ module alu
 
 always_comb begin
     case (ctrl)
-        ALU_ADD: result = A + B;
-        ALU_SUB: result = A - B;
-        ALU_SLL: 
-        ALU_SLT: // Set of Less Than (Signed)
-        ALU_SLTU: // (Unsigned)
-        ALU_SRL:
-        ALU_SRA:
-        ALU_OR:
-        ALU_AND:
-        ALU_PASS: result = B; // Pass-through (operand B)
-
-
-
+        ALU_ADD : result = A + B;
+        ALU_SUB : result = A - B;
+        ALU_SLL : result = A << B[REG_ADDR_LEN-1:0];
+        ALU_SLT : result = 32'($signed(A) < $signed(B));       // Set on Less Than (Signed)
+        ALU_SLTU: result = 32'(A < B);                         // (Unsigned)
+        ALU_SRL : result = A >> B[REG_ADDR_LEN-1:0];
+        ALU_SRA : result = $signed(A) >>> B[REG_ADDR_LEN-1:0];
+        ALU_OR  : result = A | B;                               // Bitwise OR
+        ALU_AND : result = A & B;                               // Bitwise AND
+        ALU_XOR : result = A ^ B;                               // Bitwise
+        ALU_PASS: result = B;                                   // Pass-through (operand B)
+        default : result = '0;
     endcase
 
     zero = (result == '0); // Zero flag assertion, '0 is unbased and unsized
