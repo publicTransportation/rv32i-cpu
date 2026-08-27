b880508b4d14c88f69fae98e2bf610ed2539dada
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-08-27 18:15:22 -0400

    feat(imem): modelled ROM in tb that will load program hex at simulation start

--- tb/imem.sv
+++ tb/imem.sv
@@ -1,15 +1,17 @@
 module imem
-    import rv32i_pkg::*; // Out of scope?
+    import rv32i_pkg::*; // Out of scope? // Just need Makefile
 #(
     parameter MEM_DEPTH = 512,
-    parameter HEX_PATH = "" // Compiled machine code filepath  
+    parameter HEX_PATH = "../sim/program.hex" // Compiled machine code filepath  
 )(
     input logic [XLEN-1:0] imem_addr,
     output logic [ILEN-1:0] imem_instr
 );
-logic [ILEN-1:0] mem [0:MEM_DEPTH-1]; // Unpacked array modeling Read-Only Memory (ROM)
+logic [ILEN-1:0] mem [0:MEM_DEPTH-1]; // Unpacked array modelling Read-Only Memory (ROM)
+
 initial begin
     $readmemh(HEX_PATH, mem);
 end
 
+assign imem_instr = mem[imem_addr[XLEN-1:2]]; // Dropped last 2 LSB's to divide by 4 (Byte-Addressed, Instructions are words)
 endmodule
