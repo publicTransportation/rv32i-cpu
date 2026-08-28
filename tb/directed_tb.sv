a5872224271c4f963d3dad4c95843b9813585afc
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-08-27 23:13:54 -0400

    wip(directed_tb): add instances of DUT and surround blocks and add signal declarations

--- tb/directed_tb.sv
+++ tb/directed_tb.sv
@@ -1,15 +1,57 @@
 // Procedural directed testbench
-module directed_tb 
+module directed_tb;
     import rv32i_pkg::*;
-#(
-)(
-    
-);
+
+// --- SIGNAL DECLARATIONS ---
+logic            clk;
+logic            rst_n;
+// Instruction memory interface
+logic [XLEN-1:0] imem_addr;
+logic [XLEN-1:0] imem_instr;
+// Data memory interface
+logic [XLEN-1:0] dmem_addr;
+logic [XLEN-1:0] dmem_wdata;
+logic            dmem_we;
+logic            dmem_re;
+logic [XLEN-1:0] dmem_rdata;
+
 
 // Generate clock, active-low reset
+initial clk = 1'b0; // Initalize clock to low
+always #5 clk = ~clk; // Toggle clock every 5 units
+
+initial begin
+    
+end
 
 // Instantiate DUT and surrounding blocks (imem, dmem)
+imem u_imem (
+    .imem_addr  (imem_addr),
+    .imem_instr (imem_instr) // Only output
+);
 
+dmem u_dmem (
+    .clk        (clk),
+    .dmem_addr  (dmem_addr),
+    .dmem_wdata (dmem_wdata),
+    .dmem_we    (dmem_we),
+    .dmem_re    (dmem_re),
+    .dmem_rdata (dmem_rdata) // Only output
+);
+
+core_single DUT (
+    // Inputs
+    .clk        (clk),
+    .rst_n      (rst_n),
+    .imem_instr (imem_instr),
+    .dmem_rdata (dmem_rdata),
+    // Outputs
+    .imem_addr  (imem_addr),
+    .dmem_addr  (dmem_addr),
+    .dmem_wdata (dmem_wdata),
+    .dmem_we    (dmem_we),
+    .dmem_re    (dmem_re)
+);
 
 // Monitor
 
