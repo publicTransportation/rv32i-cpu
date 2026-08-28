0337bdacdfe47626fd4b86b72f17d663e9ab116c
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-08-28 00:15:25 -0400

    feat(directed_tb): add waveform dumping, signal change tracking, timeout watchdog, deassert rst_n at sim start

--- tb/directed_tb.sv
+++ tb/directed_tb.sv
@@ -1,3 +1,4 @@
+`timescale 1ns / 1ps
 // Procedural directed testbench
 module directed_tb;
     import rv32i_pkg::*;
@@ -15,13 +16,14 @@ logic            dmem_we;
 logic            dmem_re;
 logic [XLEN-1:0] dmem_rdata;
 
-
 // Generate clock, active-low reset
 initial clk = 1'b0; // Initalize clock to low
 always #5 clk = ~clk; // Toggle clock every 5 units
 
 initial begin
-    
+    rst_n = 1'b0; // Deassert reset at t=0
+    #25; // Third clock rising edge since t=0
+    rst_n = @(negedge clk) 1'b1; // Avoid race conditions (rst_n is maximally delayed from posedge clk)
 end
 
 // Instantiate DUT and surrounding blocks (imem, dmem)
@@ -53,9 +55,17 @@ core_single DUT (
     .dmem_re    (dmem_re)
 );
 
+// Waveform dumping
+initial begin
+    $dumpfile("directed_tb.vcd");
+    $dumpvars(0, directed_tb); // Recursively dump hierarchical levels (top level is directed_tb)
+    #50000; // Timeout watchdog
+    $display("[TB ERROR] Simulation timeout reached!");
+    $finish;
+end
+
 // Monitor
 
-// Waveform dumping
 
 // Scoreboard
 
