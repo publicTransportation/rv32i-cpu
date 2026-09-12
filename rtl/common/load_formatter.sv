18cf7b5d146b5fb8f5ab85a410ea1563a1499cdc
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-09-12 14:14:00 -0400

    feat(load_formatter): implement module logic

--- rtl/common/load_formatter.sv
+++ rtl/common/load_formatter.sv
@@ -7,13 +7,20 @@ module load_formatter
 
     output logic [XLEN-1:0] load_data
 );
-case (funct3)
-    3'b000: load_data = (dmem_rdata >> (byte_offset * 8)) & 8'hFF; //LB
-    3'b001: load_data = //LH 
-    3'b010: load_data = dmem_rdata; //LW
-    3'b100: load_data = //LBU
-    3'b101: load_data = //LHU
-    default: load_data = dmem_rdata; // Default to LW (no exception implementation yet)
-endcase
+logic [7:0] load_byte;
+logic [15:0] load_hw;
+
+always_comb begin
+    load_byte = 8'(dmem_rdata >> (byte_offset * 8)); // Explicit size cast (8 bits), Shift trick to slice
+    load_hw = 16'(dmem_rdata >> (byte_offset * 8));
+    case (funct3)
+        3'b000: load_data = {{24{load_byte[7]}}, load_byte}; //LB
+        3'b001: load_data = {{16{load_hw[15]}}, load_hw};    //LH 
+        3'b010: load_data = dmem_rdata;                      //LW
+        3'b100: load_data = {24'b0, load_byte};              //LBU
+        3'b101: load_data = {16'b0, load_hw};                //LHU
+        default: load_data = dmem_rdata; // Default to LW (no exception implementation yet)
+    endcase
+end
 
 endmodule
