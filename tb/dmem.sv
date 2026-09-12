d0949f64f172f56e62b520739f0f099c93a3b4cb
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-09-12 15:35:59 -0400

    fix(dmem): dmem supports byte-enable mask writing

--- tb/dmem.sv
+++ tb/dmem.sv
@@ -39,17 +39,16 @@ end
 // Fixed-sized unpacked array
 localparam int MEM_DEPTH = 1024;
 logic [XLEN-1:0] mem [0:MEM_DEPTH-1];    
-wire [XLEN-1:0] word_idx = dmem_addr >> 2;  // Convert byte address to word index (divide by 4)
+wire [XLEN-1:0] word_idx = dmem_addr >> 2;  // Convert byte address to word index (divide by 4); Wire is continuous assignment
 
 always_ff @(posedge clk) begin
     // Synchronous Write
-    if (dmem_we) begin
-        if (word_idx < MEM_DEPTH) begin
-            mem[word_idx] <= dmem_wdata;
-        end
+    if (word_idx < MEM_DEPTH) begin // Boundary check
+        if (dmem_wmask[0]) mem[word_idx][ 7: 0] <= dmem_wdata[ 7: 0];
+        if (dmem_wmask[1]) mem[word_idx][15: 8] <= dmem_wdata[15: 8];
+        if (dmem_wmask[2]) mem[word_idx][23:16] <= dmem_wdata[23:16];
+        if (dmem_wmask[3]) mem[word_idx][31:24] <= dmem_wdata[31:24];
     end
-
-        
 end
 
 always_comb begin
