32edb0fdafe763d588768b476d59b2087eb6416f
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-09-12 10:05:10 -0400

    feat(control): add expanded LUT for more control signals

--- rtl/common/control_unit.sv
+++ rtl/common/control_unit.sv
@@ -1,7 +1,7 @@
 module control_unit
     import rv32i_pkg::*;
 (
-    input rv32i_pkg::opcode_e opcode,
+    input opcode_e opcode,
 
     output logic branch,
     output logic mem_read,
@@ -16,13 +16,35 @@ module control_unit
 
 always_comb begin
     case (opcode) // Lookup Table (likely synthesizes decoders); control unit can also use logic from a minimized truth table
-        OPCODE_OP:     {branch, mem_read, mem_to_reg, alu_op, mem_write, alu_src, reg_write} = {1'b0, 1'b0, 1'b0, ALU_OP_RTYPE,  1'b0, 1'b0, 1'b1};
-        OPCODE_LOAD:   {branch, mem_read, mem_to_reg, alu_op, mem_write, alu_src, reg_write} = {1'b0, 1'b1, 1'b1, ALU_OP_MEM,    1'b0, 1'b1, 1'b1};
-        OPCODE_STORE:  {branch, mem_read, mem_to_reg, alu_op, mem_write, alu_src, reg_write} = {1'b0, 1'b0, 1'b0, ALU_OP_MEM,    1'b1, 1'b1, 1'b0}; // Note mem_to_reg is a don't care
-        OPCODE_BRANCH: {branch, mem_read, mem_to_reg, alu_op, mem_write, alu_src, reg_write} = {1'b1, 1'b0, 1'b0, ALU_OP_BRANCH, 1'b0, 1'b0, 1'b0}; // Note mem_to_reg is a don't care
-        OPCODE_OP_IMM: {branch, mem_read, mem_to_reg, alu_op, mem_write, alu_src, reg_write} = {1'b0, 1'b0, 1'b0, ALU_OP_RTYPE,  1'b0, 1'b1, 1'b1};
-        //OPCODE_JAL:    {branch, mem_read, mem_to_reg, alu_op, mem_write, alu_src, reg_write} = 8'b1_0_0_10_0_1_1; // wip // likely MORE control signals needed to support JAL, JALR, AUIPC, LUI
-        default:       {branch, mem_read, mem_to_reg, alu_op, mem_write, alu_src, reg_write} = {1'b0, 1'b0, 1'b0, ALU_OP_MEM,    1'b0, 1'b0, 1'b0}; 
+        OPCODE_OP:             {branch, mem_read, wb_src,        alu_op,        mem_write, alu_src, reg_write, pc_src} = 
+                               {1'b0,   1'b0,     WB_SRC_ALU,    ALU_OP_RTYPE,  1'b0,      1'b0,    1'b1,      PC_SRC_NEXT};
+
+        OPCODE_OP_IMM:         {branch, mem_read, wb_src,        alu_op,        mem_write, alu_src, reg_write, pc_src} = 
+                               {1'b0,   1'b0,     WB_SRC_ALU,    ALU_OP_RTYPE,  1'b0,      1'b1,    1'b1,      PC_SRC_NEXT};
+
+        OPCODE_LOAD:           {branch, mem_read, wb_src,        alu_op,        mem_write, alu_src, reg_write, pc_src} = 
+                               {1'b0,   1'b1,     WB_SRC_MEM,    ALU_OP_MEM,    1'b0,      1'b1,    1'b1,      PC_SRC_NEXT};
+
+        OPCODE_STORE:          {branch, mem_read, wb_src,        alu_op,        mem_write, alu_src, reg_write, pc_src} = 
+                               {1'b0,   1'b0,     WB_SRC_ALU,    ALU_OP_MEM,    1'b1,      1'b1,    1'b0,      PC_SRC_NEXT};
+
+        OPCODE_BRANCH:         {branch, mem_read, wb_src,        alu_op,        mem_write, alu_src, reg_write, pc_src} = 
+                               {1'b1,   1'b0,     WB_SRC_ALU,    ALU_OP_BRANCH, 1'b0,      1'b0,    1'b0,      PC_SRC_BR_TAR};
+
+        OPCODE_JAL:            {branch, mem_read, wb_src,        alu_op,        mem_write, alu_src, reg_write, pc_src} = 
+                               {1'b0,   1'b0,     WB_SRC_PCNEXT, ALU_OP_MEM,    1'b0,      1'b0,    1'b1,      PC_SRC_BR_TAR};
+
+        OPCODE_JALR:           {branch, mem_read, wb_src,        alu_op,        mem_write, alu_src, reg_write, pc_src} = 
+                               {1'b0,   1'b0,     WB_SRC_PCNEXT, ALU_OP_MEM,    1'b0,      1'b1,    1'b1,      PC_SRC_JALR};
+
+        OPCODE_LUI:            {branch, mem_read, wb_src,        alu_op,        mem_write, alu_src, reg_write, pc_src} = 
+                               {1'b0,   1'b0,     WB_SRC_IMM,    ALU_OP_MEM,    1'b0,      1'b0,    1'b1,      PC_SRC_NEXT};
+
+        OPCODE_AUIPC:          {branch, mem_read, wb_src,        alu_op,        mem_write, alu_src, reg_write, pc_src} = 
+                               {1'b0,   1'b0,     WB_SRC_ALU,    ALU_OP_MEM,    1'b0,      1'b0,    1'b1,      PC_SRC_NEXT};
+
+        default:               {branch, mem_read, wb_src,        alu_op,        mem_write, alu_src, reg_write, pc_src} = 
+                               {1'b0,   1'b0,     WB_SRC_ALU,    ALU_OP_MEM,    1'b0,      1'b0,    1'b0,      PC_SRC_NEXT};
     endcase
 end
 
