bef694dc585416199d216c2ed19fee7a010a0ebe
Andrew Liu <aliu4517@terpmail.umd.edu>
2026-09-12 16:09:49 -0400

    fix(alu_control): explicit casting to enum type, and fixed logic issue with is_rtype

--- rtl/common/alu_control.sv
+++ rtl/common/alu_control.sv
@@ -15,12 +15,12 @@ always_comb begin // ALUOp mapping (ALUOp = 1X, X1, 00)
 
     if (alu_op[1]) begin
         case (funct3)
-            3'b000:  alu_ctrl = (bit30 && !is_rtype) ? ALU_SUB : ALU_ADD; // SUB only for R-type (I-type instructions do NOT have a funct7)
+            3'b000:  alu_ctrl = alu_ctrl_e'((bit30 && is_rtype) ? ALU_SUB : ALU_ADD); // SUB only for R-type (I-type instructions do NOT have a funct7)
             3'b001:  alu_ctrl = ALU_SLL;
             3'b010:  alu_ctrl = ALU_SLT;   // Handles SLT and SLTI
             3'b011:  alu_ctrl = ALU_SLTU;  // Handles SLTU and SLTIU
             3'b100:  alu_ctrl = ALU_XOR;
-            3'b101:  alu_ctrl = bit30 ? ALU_SRA : ALU_SRL; // SRAI / SRA use bit 30 of instr
+            3'b101:  alu_ctrl = alu_ctrl_e'(bit30 ? ALU_SRA : ALU_SRL); // SRAI / SRA use bit 30 of instr
             3'b110:  alu_ctrl = ALU_OR;
             3'b111:  alu_ctrl = ALU_AND;
             default: alu_ctrl = ALU_ADD;
